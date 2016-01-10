5900d6a3509b03fc2c0a6e7ca342a7bfa58252cd
odraudek99 <odraudek99@gmail.com>
2016-01-09 18:52:09 -0600

    fix mail service

--- src/main/java/mx/com/odraudek99/notificaciones/MailService.java
+++ src/main/java/mx/com/odraudek99/notificaciones/MailService.java
@@ -19,34 +19,33 @@ import twitter4j.Status;
 @Component
 public class MailService {
 
-	
-
 	@Value("${password}")
 	String password;
-	
+
 	@Value("${correoSalida}")
 	String correoSalida;
-	
+
 	@Value("${correoDestino}")
 	String correoDestino;
-	
+
 	public int enviarMensaje(QueryResult result) {
 
-		
-	    StringBuffer sb = new StringBuffer();
-	    
-	    for (Status status : result.getTweets()) {
-	    	sb.append("---------\n");
-	    	sb.append(status.getCreatedAt()+" @" + status.getUser().getScreenName() + ":" + status.getText()+"\n");
-	    }
-	    
-		
+		StringBuffer sb = new StringBuffer();
+
+		for (Status status : result.getTweets()) {
+			sb.append("---------\n");
+			sb.append(status.getCreatedAt() + " @" + status.getUser().getScreenName() + ":" + status.getText() + "\n");
+		}
+
+		System.out.println("correoSalida: " + correoSalida);
+
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
 
 		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
@@ -61,7 +60,7 @@ public class MailService {
 
 			message.setFrom(new InternetAddress(correoSalida));
 			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(correoDestino));
-			message.setSubject("Consulta Tweitter");
+			message.setSubject("Consulta Twitter");
 			message.setText(sb.toString());
 
 			Transport.send(message);
